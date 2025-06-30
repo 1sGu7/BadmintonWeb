baef7ee15242099d79be714d935e1315c4d1125e
1sGu7 <22dh110298@st.huflit.edu.vn>
2025-06-30 16:35:08 +0700

    Update server.js

--- app/server.js
+++ app/server.js
@@ -2,26 +2,20 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
+const fileUpload = require('express-fileupload');
 const productRoutes = require('./routes/productRoutes');
-const uploadRoutes = require('./routes/uploadRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Kết nối MongoDB
-console.log('Attempting to connect to MongoDB with URI:', process.env.MONGODB_URI);
-
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
-})
-.then(() => console.log('✅ Connected to MongoDB'))
-.catch(err => {
-  console.error('❌ MongoDB connection error:', err);
-  process.exit(1); // Thoát nếu kết nối thất bại
 });
 
 // Middleware
+app.use(fileUpload());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
@@ -29,7 +23,6 @@ app.set('view engine', 'ejs');
 
 // Routes
 app.use('/products', productRoutes);
-app.use('/upload', uploadRoutes);
 
 app.get('/', (req, res) => res.redirect('/products'));
 
